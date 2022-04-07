e04e30ac379526a494e9ac83196914cca8d02bbb
José Quintana Martín <jquintanamartin@hotmail.com>
2022-04-07 14:01:39 -0300

    Camera follows the player within world bounds

---
+++ Bouncing Hero/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset, minValues, maxValues;
+
+    private void Start()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = target.position + offset;
+
+        float boundX = Mathf.Clamp(newPos.x, minValues.x, maxValues.x);
+        float boundY = Mathf.Clamp(newPos.y, minValues.y, maxValues.y);
+        float boundZ = Mathf.Clamp(newPos.z, minValues.z, maxValues.z);
+
+        newPos = new Vector3(boundX, boundY, boundZ);
+
+        transform.position = newPos;
+    }
+}
